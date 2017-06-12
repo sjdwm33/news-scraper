d7427c0a580b5930f31919401c036b0b5f1db46b
Sam Malefyt <samanthadwm@gmail.com>
2017-06-11 20:51:02 -0500

    initial commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
 // Requiring our Note and Article models
 var Note = require("./models/Note.js");
 var Article = require("./models/Article.js");
@@ -15,6 +16,9 @@ mongoose.Promise = Promise;
 
 // Initialize Express
 var app = express();
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Use morgan and body parser with our app
 app.use(logger("dev"));
